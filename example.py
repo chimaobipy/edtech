fdc600a761e96b23e8a44d5d69f87ee1c1360910
Amy Morrill <amymorrill@uchicago.edu>
2021-08-16 13:43:25 -0500

    Added Example Skeleton

---
+++ example.py
@@ -0,0 +1,16 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+			
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+			
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout =  
+
+if __name__ == ‘__main__’:
+		app.run_server(debug=True)
+
